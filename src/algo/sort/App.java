b038f100bae19e80c069beed81447faf2a3a984f
Jonathan Edwards <edwardjd@colorado.edu>
2017-04-01 13:09:48 -0600

    added insertionSort method to Sort class

--- src/algo/sort/App.java
+++ src/algo/sort/App.java
@@ -5,13 +5,22 @@ public class App {
 		
 		// Test selectionSort
 		System.out.println("**********Test selectionSort***********");
-		int[] array = {5,2,8,7,4,9,14,23,65,1};
-		display(Sort.selectionSort(array));
+		int[] selectionArray = {5,2,8,7,4,9,14,23,65,1};
+		Sort.selectionSort(selectionArray);
+		display(selectionArray);
+		
+		// Test insertionSort
+		System.out.println("**********Test insertionSort***********");
+		int[] insertionArray = {6,4,3,8,9,10,2,3,15,1};
+		Sort.insertionSort(insertionArray);
+		display(insertionArray);
+		
 	}
 	
 	public static void display(int[] array) {
 		for(int element : array) {
 			System.out.print(element + " ");
 		}
+		System.out.println();
 	}
 }
--- src/algo/sort/Sort.java
+++ src/algo/sort/Sort.java
@@ -16,4 +16,17 @@ public class Sort {
 			}
 			return array;
 		}
+		
+		public static int[] insertionSort(int[] array) {
+			for(int i = 1; i < array.length; i++) {
+				int element = array[i];
+				int j = i - 1;
+				while(j >= 0 && array[j] > element) {
+					array[j+1] = array[j];
+					j--;
+				}
+				array[j+1] = element;
+			}
+			return array;
+		}
 }
